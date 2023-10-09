9a18a27778f37f2ee094c52ed729696ea76109e4
eviv2206 <zhenya.saprin@mail.ru>
2023-10-10 01:41:53 +0300

    first commit

---
+++ src/test/java/by/brandwatch/messagebroker/MessageBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.brandwatch.messagebroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
